69a6082b0f4efaff2fd2631237b752a9d29203a0
Mohamad Bwidani <mbwedany@gmail.com>
2019-09-16 22:05:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-function Scooter(year, color, model){
+function Scooter (year, color, model){
   this.year = year;
   this.color = color;
   this.model = model;
 }
 
-function Driver
+function Driver ()
